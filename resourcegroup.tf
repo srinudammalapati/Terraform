81da307cbfab49395d6b6c50c284565439e1f52c
naresh <naresh@gmail.com>
2023-04-11 16:21:22 +0530

    added changes

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -76,7 +76,7 @@ resource "azurerm_network_interface" "nic"{
 }
 
 resource "azurerm_network_interface_security_group_association" "nic_nsgassociation" {
-  network_interface_id      = azurerm_network_interface.my_nic.id
+  network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 
   depends_on = [
